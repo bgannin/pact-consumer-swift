550b345a87f53f5c1663b2a6f752439fadf2d5ed
Brian Ganninger <bganninger@atlassian.com>
2016-08-30 13:57:23 -0700

    Replace UIKit with Foundation

--- PactConsumerSwift/PactConsumerSwift.h
+++ PactConsumerSwift/PactConsumerSwift.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Andrew Spinks. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for PactConsumerSwift.
 FOUNDATION_EXPORT double PactConsumerSwiftVersionNumber;
--- PactConsumerSwiftTests/PactObjectiveCTests.m
+++ PactConsumerSwiftTests/PactObjectiveCTests.m
@@ -1,6 +1,7 @@
 
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
 #import <XCTest/XCTest.h>
 #import "OCAnimalServiceClient.h"
 
