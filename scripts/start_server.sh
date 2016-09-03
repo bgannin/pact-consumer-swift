448e3c68612504617995c819312ad12a39a4238d
Brian Ganninger <bganninger@atlassian.com>
2016-09-02 21:14:15 -0700

    Write log and contacts into shared build directory

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p "${SYMROOT}/pacts"
+mkdir -p "${SRCROOT}/../build"
 which pact-mock-service
 
-pact-mock-service start --pact-specification-version 2.0.0 --log "${SYMROOT}/pact.log" --pact-dir "${SYMROOT}/pacts" -p 1234
+pact-mock-service start --pact-specification-version 2.0.0 --log "${SRCROOT}/../build/pact.log" --pact-dir "${SRCROOT}/../build/pacts" -p 1234
